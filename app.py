1e67897da47fe8e7c36a107d3ef0794989cc3b58
Phansivang <lukas.cheng@pathmazing.com>
2022-12-24 12:46:12 +0700

    refactor(): app.py

--- app.py
+++ app.py
@@ -1,56 +1,26 @@
-import openai
-import os
-from googletrans import Translator
+from flask import Flask, render_template, request
+from main import Chat_AI
 
-openai.api_key = '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'
-
-message_code = ['code', 'python', 'javascript', 'c++', 'node js', 'php', 'c#', 'name']
+app = Flask(__name__, template_folder='templates')
 
 
-class Chat_AI:
-    def __init__(self):
-        self.openai = openai
-        self.translator = Translator()
+@app.route('/')
+def home():
+    return render_template(str('index.html'))
 
-    def respond(self, message, key):
-        res = self.openai.Completion.create(model="text-davinci-003", prompt=message, temperature=0.9, max_tokens=300,
-                                            top_p=1,
-                                            frequency_penalty=0, presence_penalty=0.6, stop=['Human:', 'AI:'])
-        respond = res['choices'][0]['text']
-        if key == 'km':
-            return self.khmer_translation(respond)
-        if self.ai_info(respond):
-            return  self.ai_info(respond)
-        return respond
 
-    def khmer_translation(self, message):
-        print(message)
-        if 'My name'  in message:
-            return self.ai_info(message)
-        return self.translator.translate(message, dest='km')
+@app.route("/get/respond", )
+def get_bot_response():
+    message = request.args.get('msg')
+    return Chat_AI().translation(message)
 
-    def translation(self,message):
-        detect_lange =  self.detection_lang(message)
-        if detect_lange == 'km':
-            translate =  self.translator.translate(message, dest='en')
-            return  self.respond(translate, detect_lange)
-        return self.respond(message, detect_lange)
 
-    def detection_lang(self, message):
-        return  self.translator.detect(message)
+@app.route('/v1/message', methods=['GET'])
+def get():
+    request_data = request.get_json()
+    message = request_data['message']
+    return Chat_AI().translation(message)
 
-    def ai_info(self, res):
-        info = 'My name is Peav monivireak pech, AKA Richie, and I am a full-stack developer currently studying computer science at CADT and fintech at TGI.'
-        if self.detection_lang(res) == 'en':
-            if 'My name' in res:
-                return  info
-        pass
 
-    def greeting_validation(self, message):
-        key_words = ['hello ', 'hi ', 'What up ', "What's up ", 'Hey ']
-        for key_word in key_words:
-            if message in key_word:
-                if self.detection_lang(message) == 'km':
-                    return message.__add__('លោក')
-                return message.__add__('Bot')
-        pass
+if __name__ == "__main__":
+    app.run(debug=True)
--- main.py
+++ main.py
@@ -1,25 +1,55 @@
-from flask import Flask, render_template, request
-from ai_service import Chat_AI
+import openai
+from googletrans import Translator
 
-class AI_Chat:
+openai.api_key = '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'
+
+# message_code = ['code', 'python', 'javascript', 'c++', 'node js', 'php', 'c#', 'name']
+
+
+class Chat_AI:
     def __init__(self):
-        self.app = Flask(__name__, template_folder='templates')
+        self.openai = openai
+        self.translator = Translator()
 
-        @self.app.route('/')
-        def home():
-            return render_template(str('index.html'))
+    def respond(self, message, key):
+        res = self.openai.Completion.create(model="text-davinci-003", prompt=message, temperature=0.9, max_tokens=300,
+                                            top_p=1,
+                                            frequency_penalty=0, presence_penalty=0.6, stop=['Human:', 'AI:'])
+        respond = res['choices'][0]['text']
+        if key == 'km':
+            return self.khmer_translation(respond)
+        if self.ai_info(respond):
+            return self.ai_info(respond)
+        return respond
 
-        @self.app.route("/get/respond",)
-        def get_bot_response():
-            message = request.args.get('msg')
-            return Chat_AI().translation(message)
-        @self.app.route('/v1/message',methods=['GET'])
-        def get():
-            request_data = request.get_json()
-            message = request_data['message']
-            return  Chat_AI().translation(message)
+    def khmer_translation(self, message):
+        if 'My name' in message:
+            return self.ai_info(message)
+        return self.translator.translate(message, dest='km').text
 
+    def translation(self, message):
+        detect_lange = self.detection_lang(message)
+        if detect_lange == 'km':
+            translate = self.translator.translate(message, dest='en').text
+            return self.respond(translate, detect_lange)
+        return self.respond(message, detect_lange)
 
+    def detection_lang(self, message):
+        return self.translator.detect(message).lang
 
-if __name__ == "__main__":
-    AI_Chat().app.run(debug=True)
+    def ai_info(self, res):
+        info = 'My name is Peav monivireak pech, AKA Richie, and I am a full-stack developer currently studying ' \
+               'computer science at CADT and fintech at TGI. '
+        if self.detection_lang(res) == 'en':
+            if 'My name' in res:
+                return info
+        pass
+
+    def greeting_validation(self, message):
+        key_words = ['hello ', 'hi ', 'What up ', "What's up ", 'Hey ']
+        for key_word in key_words:
+            if message in key_word:
+                if self.detection_lang(message) == 'km':
+                    return message.__add__('លោក')
+                return message.__add__('Bot')
+        pass
