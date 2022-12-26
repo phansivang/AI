872bb99b5cfdd48af7d17bbb7891058015d117f0
Phansivang <lukas.cheng@pathmazing.com>
2022-12-26 17:36:36 +0700

    refactor(): validate greeting and add data json

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 import openai
 import os
 from googletrans import Translator
+import json
 
 openai.api_key = os.environ.get("OPENAI_API")
 
 
 # message_code = ['code', 'pytho', 'javascript', 'c++', 'node js', 'php', 'c#', 'nname']
 
-
 class Chat_AI:
     def __init__(self):
         self.openai = openai
         self.translator = Translator()
+        self.json = json.load(open('data.json', encoding="utf8"))
 
     def respond(self, message, key):
         res = self.openai.Completion.create(model="text-davinci-003", prompt=message, temperature=0.9, max_tokens=300,
@@ -25,31 +26,31 @@ class Chat_AI:
 
     def khmer_translation(self, message):
         if 'My name' in message:
-            return 'នាងខ្ញុំឈ្មោះ ពាវ មុនីវីរៈពេជ្រ'
+            return self.json['data']['nameKh']
         return self.translator.translate(message, dest='km').text
 
     def translation(self, message):
-        detect_lange = self.detection_lang(message)
+        greeting = self.greeting_validation(message)
+        print(greeting)
+        detect_lange = self.detection_lang(greeting)
         if detect_lange == 'km':
-            translate = self.translator.translate(message, dest='en').text
+            translate = self.translator.translate(greeting, dest='en').text
             return self.respond(translate, detect_lange)
-        return self.respond(message, detect_lange)
+        return self.respond(greeting, detect_lange)
 
     def detection_lang(self, message):
         return self.translator.detect(message).lang
 
     def check_ai_name(self, res, key):
-        info = 'My name is Peav monivireak pech, AKA Richie, and I am a full-stack developer currently studying ' \
-               'computer science at CADT and fintech at TGI. '
+        info = self.json
+        print(info)
         if 'My name' in res:
             if key == 'en':
-                return info
+                return info['data']['nameEn']
 
     def greeting_validation(self, message):
-        key_words = ['hello ', 'hi ', 'What up ', "What's up ", 'Hey ']
+        key_words = ['hello ', 'hi', 'What up ', "What's up ", 'Hey ','សួស្ដី','សួស្ដីបង']
         for key_word in key_words:
             if message in key_word:
-                if self.detection_lang(message) == 'km':
-                    return message.__add__('លោក')
-                return message.__add__('Bot')
-        pass
+                return 'Hello madam?'
+            return message
