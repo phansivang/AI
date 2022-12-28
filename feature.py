8810e5fc8041db7960b23bd95c9700b1e5a1d524
Phansivang <lukas.cheng@pathmazing.com>
2022-12-28 20:48:30 +0700

    fix(): download path

--- feature.py
+++ feature.py
@@ -1,11 +1,12 @@
 from pytube import YouTube
-import os
+
 
 def download_youtube_video(link):
     youtubeObject = YouTube(link)
 
     try:
-        youtubeObject.streams.get_by_resolution('720p').download(os.path.expanduser("~/Downloads"))
+        youtubeObject.streams.first()
+        youtubeObject.streams.get_by_resolution('720p').download(output_path='/dev/null')
     except:
         return 'An error has occurred'
     return 'Download is completed successfully'
