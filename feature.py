b7985bf134f17e7a46f6aa5463748d8295be20a5
phansivang <lukas.cheng@pathmazing.com>
2022-12-29 13:30:05 +0700

    fix: pytube

--- feature.py
+++ feature.py
@@ -1,12 +1,14 @@
 from pytube import YouTube
-
+import os
 
 def download_youtube_video(link):
     youtubeObject = YouTube(link)
 
     try:
-        youtubeObject.streams.first()
-        youtubeObject.streams.get_by_resolution('720p').download(output_path='/dev/null')
+
+        youtube = youtubeObject.streams.first()
+        youtube.download(os.path.expanduser("~/Downloads"))
+        # youtubeObject.streams.get_by_resolution('720p').download(output_path='/dev/null')
     except:
         return 'An error has occurred'
     return 'Download is completed successfully'
