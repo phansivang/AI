32ff7c0a3b0b2a3ed65acb3e3142053a92eaae3b
Phansivang <lukas.cheng@pathmazing.com>
2022-12-30 19:00:07 +0700

    fix(): download video youtube

--- feature.py
+++ feature.py
@@ -1,12 +1,13 @@
 from pytube import YouTube
 import os
 
+
 def download_youtube_video(link):
     youtubeObject = YouTube(link)
 
     try:
 
-        youtube = youtubeObject.streams.first()
+        youtube = youtubeObject.streams.first().download(os.environ['\Downloads'])
         youtube.download(youtube.title)
         # youtubeObject.streams.get_by_resolution('720p').download(output_path='/dev/null')
     except:
