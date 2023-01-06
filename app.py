65833e0902e0205f97b7d9b5f4e77f5ee8a51aef
phansivang <lukas.cheng@pathmazing.com>
2023-01-06 15:21:16 +0700

    ban user ip

--- app.py
+++ app.py
@@ -1,15 +1,21 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, Response
 from main import chat_ai
 from flask_sslify import SSLify
+import os
 
 app = Flask(__name__, template_folder='templates')
 sslify = SSLify(app)
-
+ips =  [os.environ.get("OPENAI_API")]
 
 @app.route('/')
 def home():
     return render_template(str('index.html'))
 
+@app.before_request
+def block_ip():
+
+  if request.remote_addr in ips:
+    return Response("Not allowed IP", status=403)
 
 @app.route("/get/respond", )
 def get_bot_response():
